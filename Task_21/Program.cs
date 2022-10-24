f51e7cf6d68fa168f6a81b55aef63cad544a8c2f
Tatyana <t666ax89@yandex.ru>
2022-10-24 20:52:10 +0300

    поменяла int.Parse на Convert.ToInt32

--- Task_21/Program.cs
+++ Task_21/Program.cs
@@ -4,22 +4,22 @@
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
 Console.WriteLine("Введите число");
-int X1 = int.Parse(Console.ReadLine());
+int X1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите число");
-int Y1 = int.Parse(Console.ReadLine());
+int Y1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите число");
-int Z1 = int.Parse(Console.ReadLine());
+int Z1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите число");
-int X2 = int.Parse(Console.ReadLine());
+int X2 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите число");
-int Y2 = int.Parse(Console.ReadLine());
+int Y2 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите число");
-int Z2 = int.Parse(Console.ReadLine());
+int Z2 = Convert.ToInt32(Console.ReadLine());
 
 double d = Math.Sqrt(Math.Pow(X1-X2,2)+Math.Pow(Y1-Y2,2)+Math.Pow(Z1-Z2,2));
 Console.WriteLine(d);
--- Task_23/Program.cs
+++ Task_23/Program.cs
@@ -4,7 +4,7 @@
 // 5 -> 1, 8, 27, 64, 125
 
 Console.WriteLine("Введите число N");
-int num = int.Parse(Console.ReadLine());
+int num = Convert.ToInt32(Console.ReadLine());
 
 for(int i=1; i<=num; i++)
 {
